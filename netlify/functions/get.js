703417944ec6b8e8569beb8ffd63be8051b1cad3
Younnes Ansouri <yan@radionomy.com>
2022-12-25 18:49:44 +0100

    use node-fetch instead of native fetch

--- netlify/functions/get.js
+++ netlify/functions/get.js
@@ -3,6 +3,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+import fetch from 'node-fetch';
 
 const router = express.Router();
 router.get('/', async (req, res) => {
